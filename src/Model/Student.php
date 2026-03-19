c68e717915666b207e82011d8675dce13419264c
ianadou <ianadou807@gmail.com>
2026-03-19 12:36:28 +0100

    feat(model): ajouter le modèle Student

---
+++ src/Model/Student.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+class Student
+{
+    public function __construct(
+        public readonly int $id,
+        public string $firstName,
+        public string $lastName,
+        public string $email,
+        public float $grade,
+        public string $field,
+    ) {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+            'email' => $this->email,
+            'grade' => $this->grade,
+            'field' => $this->field,
+        ];
+    }
+}
